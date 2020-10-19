45969c8d672df53d1d8b981266d4f821bd6cf7b6
mustafakaplan <mstfakapln@gmail.com>
2020-10-19 13:32:37 +0300

    building-React&Spring

---
+++ ws/src/main/resources/static/precache-manifest.44581d424a082b225f018563e14539ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "259cdb63308dcd567329dde0752aecf0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b31f7fb7e97991792da",
+    "url": "/static/css/main.630ea620.chunk.css"
+  },
+  {
+    "revision": "1c0c129cd070a901b3d6",
+    "url": "/static/js/2.b4533514.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b4533514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b31f7fb7e97991792da",
+    "url": "/static/js/main.e95455ea.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "2dcb4821b18b3e6060236b90b13c136b",
+    "url": "/static/media/hoaxify.2dcb4821.png"
+  },
+  {
+    "revision": "926fda932fcddf9dae79b8a3a560745b",
+    "url": "/static/media/profile.926fda93.png"
+  }
+]);
